892ec1ab4166c44920cc00245d92ef384fc14711
Maxim Dmitriev <maximv.dmitriev@gmail.com>
2019-12-04 13:14:18 +0300

    add draft  for 4-module 2-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,6 +1,11 @@
+/* eslint-disable arrow-parens */
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
+
+const LIMIT = 1e6;
 
 const server = new http.Server();
 
@@ -11,6 +16,44 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      if (req.headers['content-length'] > LIMIT) {
+        res.statusCode = 413;
+        res.end();
+        return;
+      }
+      if (pathname.indexOf('/') > -1) {
+        console.log('/');
+        res.statusCode = 400;
+        res.end();
+        return;
+      }
+      const writeStream = fs.createWriteStream(filepath, {flags: 'wx'});
+      const limitSizeStream = new LimitSizeStream({limit: LIMIT});
+      req.pipe(limitSizeStream).pipe(writeStream);
+      limitSizeStream.on('error', err => {
+        console.log('exeed');
+        res.statusCode = 413;
+        res.end();
+        fs.unlink(filepath, () => {});
+        return;
+      });
+      writeStream.on('error', err => {
+        console.log('err');
+        if (err.code === 'EEXIST') {
+          res.statusCode = 409;
+          res.end();
+          return;
+        }
+        res.statusCode = 500;
+        res.end();
+        fs.unlink(filepath, () => {});
+        return;
+      });
+      writeStream.on('close', () => {
+        res.statusCode = 201;
+        console.log('close');
+        res.end();
+      });
 
       break;
 
