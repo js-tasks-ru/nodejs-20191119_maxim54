2894b2130817eafeb02463e91cdab51906ce4f2a
Maxim Dmitriev <maximv.dmitriev@gmail.com>
2019-12-10 21:20:02 +0300

    add solution for 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable arrow-parens */
 const Koa = require('koa');
 const app = new Koa();
 
@@ -7,10 +8,25 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let requests = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  const promise = new Promise(function(resolve, reject) {
+    requests.push(resolve);
+  });
+
+  const message = await promise;
+  ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+  if (ctx.request.body.message) {
+    requests.forEach(res => {
+      res(ctx.request.body.message);
+    });
+    requests = [];
+    ctx.body = '200';
+  }
 });
 
 app.use(router.routes());
